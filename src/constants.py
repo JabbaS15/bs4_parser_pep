469f6dddec1683884f84524bccb045ed20d536c0
Jabba <JabbaS15@yandex.kz>
2022-10-10 22:25:49 +0600

    ver 0.1

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
